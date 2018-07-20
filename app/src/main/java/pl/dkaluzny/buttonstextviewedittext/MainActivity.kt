740f3f94ccb8cde17af210003d430c7144d7f93d
kalug89 <kaluzny_david@hotmail.com>
2018-07-20 23:40:44 +0200

    End project, validation

--- app/src/main/java/pl/dkaluzny/buttonstextviewedittext/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/buttonstextviewedittext/MainActivity.kt
@@ -1,12 +1,18 @@
 package pl.dkaluzny.buttonstextviewedittext
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = enterNameEdit.text
+        showNameButtonbutton.setOnClickListener {
+            if (text.isEmpty()) resultTxt.text = "Enter Name" else resultTxt.text = "Welcome " + text
+        }
     }
 }
